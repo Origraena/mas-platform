8a2b23ea14709541d0f2c7d7d6b224fac0898974
wilbefast <wilbefast@gmail.com>
2012-09-27 22:47:14 +0200

    gui now creates agents in

--- src/main/wjd/gui/GUIWindow.java
+++ src/main/wjd/gui/GUIWindow.java
@@ -8,6 +8,21 @@ package wjd.gui;
 
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
+import ori.mas.actors.EatActor;
+import ori.mas.actors.MovementActor;
+import ori.mas.core.AdaptedScene;
+import ori.mas.core.Agent;
+import ori.mas.core.Body;
+import ori.mas.core.DefaultHeart;
+import ori.mas.core.Properties;
+import ori.mas.core.World;
+import ori.mas.fsm.StateMachineMind;
+import ori.mas.fsm.states.PatrolState;
+import ori.mas.fsm.states.PredateState;
+import ori.mas.sensors.ShapeSensor;
+import ori.ogapi.geometry.Circle;
+import ori.ogapi.geometry.LinkedListSurface;
+import ori.ogapi.geometry.Point;
 import wjd.gui.view.DrawGL;
 import wjd.gui.view.ViewPort;
 import wjd.math.V2;
@@ -25,6 +40,7 @@ public class GUIWindow extends LWJGLWindow
   
   /// ATTRIBUTES
   private ViewPort view;
+  private World world;
 
   /// METHODS
   // construction
@@ -35,15 +51,48 @@ public class GUIWindow extends LWJGLWindow
 
     // create view
     view = new ViewPort(new V2(_width, _height));
-
+    
     // start up
     reset();
   }
 
   private void reset()
   {
-    // create view
+    // reset view
     view.reset();
+    
+    // create the world
+    world = new World(new DefaultHeart(), 
+      new AdaptedScene(new LinkedListSurface<Body>()));
+
+    // build the patrol-agent's body
+		Body body = new Body();
+		body.set(Properties.HEALTH, new Integer(1000));
+		body.set(Properties.HEALTH_MIN, new Integer(0));
+		body.set(Properties.HEALTH_MAX, new Integer(1000));
+		body.set(Properties.FEED, new Integer(100));
+		body.set(Properties.FEED_MIN, new Integer(0));
+		body.set(Properties.FEED_MAX, new Integer(100));
+		body.translate(new Point(100, 100));
+		body.addActor(new MovementActor(body, 5));
+		body.addSensor(new ShapeSensor(body, new Circle(0, 0, 150)));
+    // attach a simple mind and add to the world
+    world.add(new Agent(new StateMachineMind(new PatrolState()), body));
+    
+    // build the predate-agent's body
+		body = new Body();
+		body.set(Properties.HEALTH, new Integer(1000));
+		body.set(Properties.HEALTH_MIN, new Integer(0));
+		body.set(Properties.HEALTH_MAX, new Integer(1000));
+		body.set(Properties.FEED, new Integer(100));
+		body.set(Properties.FEED_MIN, new Integer(0));
+		body.set(Properties.FEED_MAX, new Integer(100));
+		body.translate(new Point(100, 120));
+		body.addActor(new MovementActor(body, 10));
+		body.addActor(new EatActor(body, 10));
+		body.addSensor(new ShapeSensor(body, new Circle(0, 0, 100)));
+    // attach a simple mind and add to the world
+    world.add(new Agent(new StateMachineMind(new PredateState()), body));
   }
 
   // overrides
