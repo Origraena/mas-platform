02da8947593ed671452182a95595fbe5f295a96b
wilbefast <wilbefast@gmail.com>
2012-10-01 19:39:47 +0200

    GLPaintbrush now implements IPaintbrush

--- src/main/wjd/gui/view/GLPaintbrush.java
+++ src/main/wjd/gui/view/GLPaintbrush.java
@@ -24,25 +24,37 @@ import wjd.math.Rect;
 import wjd.math.V2;
 
 /**
- * A collection of drawing functions to render primitives (circles, boxes
- * and so-on) using OpenGL.
+ * A collection of drawing functions to render primitives (circles, boxes and
+ * so-on) using OpenGL.
  *
  * @author wdyce
  * @date 16-Feb-2012
  */
-public abstract class GLPaintbrush
+public class GLPaintbrush implements IPaintbrush
 {
-  /// CLASS NAMESPACE CONSTANTS
+  /* CONSTANTS */
   private static final int CIRCLE_BASE_SEGMENTS = 6;
-  private static TrueTypeFont font = null;
+  
+  /* SINGLETON */
+  private GLPaintbrush instance;
+  public GLPaintbrush getInstance()
+  {
+    if(instance == null)
+      instance = new GLPaintbrush();
+    return instance;
+  }
+  
+  /* ATTRIBUTES */
+  private org.newdawn.slick.Color slickColour;
+  private TrueTypeFont font;
 
-  /// CLASS NAMESPACE FUNCTIONS
-  // initialisation
+  /* METHODS */
+  // creation
   /**
    * Set up the OpenGL state machine for drawing, including setting clear colour
    * and depth, and enabling/disabling various options (namely 3D).
    */
-  public static void init()
+  private GLPaintbrush()
   {
     // background colour and depth
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
@@ -60,15 +72,40 @@ public abstract class GLPaintbrush
     font = new TrueTypeFont(new Font("Arial", Font.PLAIN, 12), false);
   }
 
+  // setup functions
+  @Override
+  public IPaintbrush setColour(Colour colour)
+  {
+    slickColour = new Color(colour.r, colour.g, colour.b, colour.a);
+    glColor3f(colour.r, colour.b, colour.g);
+    return this;
+  }
+
+  @Override
+  public IPaintbrush setLineWidth(float lineWidth)
+  {
+    glLineWidth(lineWidth);
+    return this;
+  }
+
+  @Override
+  public IPaintbrush setFont(Object new_font)
+  {
+    if (new_font instanceof TrueTypeFont)
+      font = (TrueTypeFont) new_font;
+    return this;
+  }
+
   // drawing functions
   /**
-   * Draw a black circle outline around the specified position, using the given
+   * Draw a circle outline around the specified position, using the given
    * radius.
    *
    * @param centre vector position corresponding to the centre of the circle.
    * @param radius the size of the circle from centre to outskirts.
    */
-  public static void circle(V2 centre, float radius)
+  @Override
+  public void circle(V2 centre, float radius)
   {
     int deg_step = (int) (360 / (CIRCLE_BASE_SEGMENTS * radius));
     glBegin(GL_TRIANGLE_FAN);
@@ -83,26 +120,13 @@ public abstract class GLPaintbrush
   }
 
   /**
-   * Draw a circle at around the specified position, using the given radius and
-   * Colour.
-   *
-   * @param centre vector position corresponding to the centre of the circle.
-   * @param radius the size of the circle from centre to outskirts.
-   * @param colour the Colour to be used to draw the circle.
-   */
-  public static void circle(V2 centre, float radius, Colour colour)
-  {
-    glColor3f(colour.r, colour.b, colour.g);
-    circle(centre, radius);
-  }
-
-  /**
    * Draw a straight black line between the two specified points.
    *
    * @param start vector position corresponding to the start of the line.
    * @param end vector position corresponding to the end of the line.
    */
-  public static void line(V2 start, V2 end)
+  @Override
+  public void line(V2 start, V2 end)
   {
     glBegin(GL_LINES);
     glVertex2d(start.x(), start.y());
@@ -111,27 +135,12 @@ public abstract class GLPaintbrush
   }
 
   /**
-   * Draw a straight line between the two specified points using the specified
-   * Colour and the specified line width.
-   *
-   * @param start vector position corresponding to the start of the line.
-   * @param end vector position corresponding to the end of the line.
-   * @param colour Colour to be used to draw the line.
-   * @param width the width of the line in pixels.
-   */
-  public static void line(V2 start, V2 end, Colour colour, float width)
-  {
-    glLineWidth(width);
-    glColor3f(colour.r, colour.b, colour.g);
-    line(start, end);
-  }
-
-  /**
-   * Draw the outline of a Rectangle in black.
+   * Draw the outline of a Rectangle.
    *
    * @param rect the rectangle object whose outline will be drawn.
    */
-  public static void box(Rect rect)
+  @Override
+  public void box(Rect rect)
   {
     glBegin(GL_QUADS);
     glVertex2f(rect.x(), rect.y());
@@ -142,43 +151,16 @@ public abstract class GLPaintbrush
   }
 
   /**
-   * Draw the outline of a Rectangle using the specified Colour.
-   *
-   * @param rect the rectangle object whose outline will be drawn.
-   * @param colour the Colour to be used to draw the rectangle.
-   */
-  public static void box(Rect rect, Colour colour)
-  {
-    glColor3f(colour.r, colour.b, colour.g);
-    box(rect);
-  }
-
-  /**
-   * Draw a String of characters at the indicated position in black.
+   * Draw a String of characters at the indicated position.
    *
    * @param string the String of characters to be drawn.
    * @param position the position on the screen to draw the String.
    */
-  public static void text(String string, V2 position)
+  @Override
+  public void text(String string, V2 position)
   {
     glEnable(GL_BLEND);
-    font.drawString(position.x(), position.y(), string, Color.black);
-    glDisable(GL_BLEND);
-  }
-
-  /**
-   * Draw a String of characters at the indicated position using the specified
-   * Colour.
-   *
-   * @param string the String of characters to be drawn.
-   * @param position the position on the screen to draw the String.
-   * @param colour the Colour to be used to draw the String.
-   */
-  public void text(String string, V2 position, Colour colour)
-  {
-    glEnable(GL_BLEND);
-    font.drawString(position.x(), position.y(), string,
-      new Color(colour.r, colour.g, colour.b));
+    font.drawString(position.x(), position.y(), string, slickColour);
     glDisable(GL_BLEND);
   }
 }
